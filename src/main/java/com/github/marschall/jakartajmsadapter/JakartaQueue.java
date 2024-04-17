b667d157fb897439e7e9482d2ff3756e85af14b6
Philippe Marschall <philippe.marschall@gmail.com>
2024-04-17 06:44:41 +0200

    Make Queue public
    Fixes #3

--- src/main/java/com/github/marschall/jakartajmsadapter/JakartaQueue.java
+++ src/main/java/com/github/marschall/jakartajmsadapter/JakartaQueue.java
@@ -3,12 +3,17 @@ package com.github.marschall.jakartajmsadapter;
 import jakarta.jms.JMSException;
 import jakarta.jms.Queue;
 
-sealed class JakartaQueue extends JakartaDestination implements Queue
+/**
+ * Adapts a Java EE {@link javax.jms.Queue} to a Jakarta EE {@link Queue}.
+ * <p>
+ * {@code public} for cases where a queue is looked up through JNDI.
+ */
+public sealed class JakartaQueue extends JakartaDestination implements Queue
   permits JakartaTemporaryQueue {
 
   private final javax.jms.Queue javaxQueue;
 
-  JakartaQueue(javax.jms.Queue javaxQueue) {
+  public JakartaQueue(javax.jms.Queue javaxQueue) {
     super(javaxQueue);
     this.javaxQueue = javaxQueue;
   }
